04c502b02c0d4136db7266f303453eef0dfdd196
belange555 <belangejan@gmail.com>
2025-07-24 15:10:05 +0200

    etape2

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello world !")
+function getComputerChoice(){
+    const liste = ["pierre", "papier", "ciseaux"]
+    let indice = Math.floor (Math.random() * 3)
+    let choix = liste[indice]
+    return choix
+} 
+getComputerChoice ()
+console.log(getComputerChoice())
