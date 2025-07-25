1ba9b3ae30ea56a00ab3b7a9c6b5888bdb14b51a
Nora Priour-Petitbon <nora.pp@orange.fr>
2025-07-25 10:44:44 +0200

    Etape 5 fini

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getComputerChoice(){
     let indice = Math.floor (Math.random() * 3)
     let choix = liste[indice]
     return choix
-} 
+}
 // getComputerChoice ()
 // console.log(getComputerChoice())
 
@@ -21,13 +21,32 @@ function playRound(humanChoice, computerChoice ){
     let resultat = " "
     if (humanChoice == computerChoice){
         resultat = "égalité"
-       }
+    }
     else if (humanChoice == "ciseaux") {
         if (computerChoice == "papier"){
             resultat = "bravo ! bravo ! Tu as coupé le papier :)"
-         } 
-        else {resultat = "trop nul, les ciseaux se sont fait éclater :("}
+        }
+        else {resultat = "trop nul, les ciseaux se sont fait éclater :("
+        }
+    }
+    else if (humanChoice == "papier") {
+        if (computerChoice == "pierre"){
+            resultat = "bravo ! bravo ! Tu as recouvert la pierre :)"
+        }
+        else {resultat = "trop nul, les ciseaux ont coupé ton papier :("
+        }
+    }
+    else {
+        if (computerChoice == "ciseaux"){
+            resultat = "bravo ! bravo ! Tu as éclaté les ciseaux :)"
+        }
+        else {resultat = "trop nul, ta pierre a été recouverte :("
+        }
     }
     return resultat
-} 
-console.log(playRound("papier", "papier"))
+}
+
+const humanSelection = getHumanChoice ()
+const computerSelection = getComputerChoice ()
+
+console.log(playRound(humanSelection, computerSelection))
